3fc4c0dfca029912357fa56bb1f4cc2d90f2b2f3
Hamnab02 <hamnab02@gmail.com>
2024-09-08 21:06:18 +0100

    moved code over from codesandbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Weather />
         <a
           className="App-link"
           href="https://reactjs.org"
